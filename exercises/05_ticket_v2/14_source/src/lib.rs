f2b5d187765249ca1786e3c54037a94c04760934
giornn0 <rusty.arrebol@proton.me>
2024-05-19 05:15:03 -0300

    chore: solve ticket_v2 source error

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -1,3 +1,5 @@
+use status::ParseStatusError;
+
 use crate::status::Status;
 
 // We've seen how to declare modules in one of the earliest exercises, but
@@ -23,6 +25,12 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 characters")]
     DescriptionTooLong,
+    #[error("{invalid_status}")]
+    ParseStatusError {
+        #[from]
+        #[source]
+        invalid_status: ParseStatusError,
+    },
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -47,12 +55,12 @@ impl Ticket {
             return Err(TicketNewError::DescriptionTooLong);
         }
 
-        // TODO: Parse the status string into a `Status` enum.
+        let parse_status = Status::try_from(status)?;
 
         Ok(Ticket {
             title,
             description,
-            status,
+            status: parse_status,
         })
     }
 }
