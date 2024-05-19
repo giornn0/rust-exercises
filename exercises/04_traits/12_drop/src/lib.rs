f68aaca8c309298c952979ef5fce5cbc233970c0
giornn0 <rusty.arrebol@proton.me>
2024-05-19 02:48:36 -0300

    chore: solve traits drop

--- exercises/04_traits/12_drop/src/lib.rs
+++ exercises/04_traits/12_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+use core::panic;
+
+struct DropBomb {
+    active: bool,
+}
+impl DropBomb {
+    fn new() -> Self {
+        Self { active: true }
+    }
+    fn defuse(&mut self) {
+        self.active = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.active {
+            panic!("The bomb has not been defused!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
