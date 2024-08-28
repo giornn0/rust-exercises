f6c02763a7ed8bacf761cba272f9e484673322b7
giornn0 <rusty.arrebol@proton.me>
2024-08-28 00:23:18 -0300

    chore: solve threads static

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,10 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let (h1, h2) = slice.split_at(slice.len() / 2);
+    let t1 = thread::spawn(|| h1.iter().sum::<i32>());
+    let t2 = thread::spawn(|| h2.iter().sum::<i32>());
+    t1.join().unwrap() + t2.join().unwrap()
 }
 
 #[cfg(test)]
